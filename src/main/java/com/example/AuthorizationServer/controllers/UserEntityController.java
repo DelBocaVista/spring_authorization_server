5cd1f2d09a20a2aba12c078604679b129d30b7cf
Meckan <meckan@hotmail.se>
2020-04-08 14:23:31 +0200

    Lagt till en verifyToken metod
    Lagt till en verifyToken metod för att kunna verifiera access tokens, denna måste kanske göras om senare men fungerar tillsvidare

--- src/main/java/com/example/AuthorizationServer/controllers/UserEntityController.java
+++ src/main/java/com/example/AuthorizationServer/controllers/UserEntityController.java
@@ -5,6 +5,7 @@ import com.example.AuthorizationServer.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -78,7 +79,10 @@ public class UserEntityController {
         userService.deleteUser(id);
     }
 
-
+    @GetMapping("/verify")
+    public @ResponseBody boolean verifyToken(){
+        return true;
+    }
     /*@GetMapping(path="/org/{id}")
     @ResponseBody
     public ResponseEntity<?> findAllByOrgId(@PathVariable Long id) {
